481d9c7b33a2b07d693febe65547be1ca337a255
Andrew Webb <webba@ihopu.org>
2021-11-15 19:02:22 -0600

    get receipts

--- src/components/receipt/ReceiptProvider.js
+++ src/components/receipt/ReceiptProvider.js
@@ -10,13 +10,17 @@ export const ReceiptProvider = (props) => {
     const [ totalsYear , setReceiptYear] = useState("")
 
     const getReceipts = () => {
-        return fetch("https://gig-tax-api.herokuapp.com/receipts")
+        return fetch("https://gig-tax-server.herokuapp.com/receipts", {
+        headers: {
+            "Authorization": `Token ${localStorage.getItem("gig-tax_user_token")}`
+        }
+    })
         .then(res => res.json())
         .then(setReceipts)
     }
 
     const addReceipt = ReceiptObj => {
-        return fetch("https://gig-tax-api.herokuapp.com/receipts", {
+        return fetch("https://gig-tax-server.herokuapp.com/receipts", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
@@ -27,19 +31,23 @@ export const ReceiptProvider = (props) => {
     }
 
     const deleteReceipt = receiptId => {
-        return fetch(`https://gig-tax-api.herokuapp.com/receipts/${receiptId}`, {
+        return fetch(`https://gig-tax-server.herokuapp.com/receipts/${receiptId}`, {
             method: "DELETE"
         })
             .then(getReceipts)
     }
 
     const getReceiptById = receiptId => {
-        return fetch(`https://gig-tax-api.herokuapp.com/receipts/${receiptId}`)
+        return fetch(`https://gig-tax-server.herokuapp.com/receipts/${receiptId}`, {
+            headers: {
+                "Authorization": `Token ${localStorage.getItem("gig-tax_user_token")}`
+            }
+        })
         .then(res => res.json())
     }
 
     const updateReceipt = receipt => {
-        return fetch(`https://gig-tax-api.herokuapp.com/receipts/${receipt.id}`, {
+        return fetch(`https://gig-tax-server.herokuapp.com/receipts/${receipt.id}`, {
           method: "PUT",
           headers: {
             "Content-Type": "application/json"
